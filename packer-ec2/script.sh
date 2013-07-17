96c495ab9bbfef50c246278a55588dd16b80bfd3
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-07-17 01:34:21 +0000

    add yum option

--- packer-ec2/script.sh
+++ packer-ec2/script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo yum install httpd php php-mbstring php-pdo php-mysql mysql-server php-xml
+sudo yum upgrade -y
+sudo yum install -y httpd php php-mbstring php-pdo php-mysql mysql-server php-xml
 
 curl -L https://www.opscode.com/chef/install.sh | sudo bash
