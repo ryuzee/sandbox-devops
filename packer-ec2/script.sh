61eab35da74916eb2839461170d28e7fef682817
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-07-24 08:10:39 +0000

    apache settings

--- packer-ec2/script.sh
+++ packer-ec2/script.sh
@@ -2,4 +2,7 @@
 sudo sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 sudo yum upgrade -y
 sudo yum install -y httpd php php-mbstring php-pdo php-mysql mysql-server php-xml
+sudo /sbin/chkconfig --level 2345 httpd on
+sudo \rm /etc/httpd/conf.d/welcome.conf
+sudo -c "echo 'OK' > /var/www/html/index.html"
 curl -L https://www.opscode.com/chef/install.sh | sudo bash
