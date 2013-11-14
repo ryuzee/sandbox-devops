40d6f1d8c742b19638f02fe56381a23efb9bc29b
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-14 14:15:22 +0900

    refactor

---
+++ vagrant-berkshelf-sensu-sample/sensu-client-wrapper/attributes/default.rb
@@ -0,0 +1,5 @@
+default["sensu"]["use_ssl"] = false
+default["sensu"]["rabbitmq"]["host"] = "localhost"
+default["sensu"]["rabbitmq"]["port"] = 5672
+default["sensu"]["rabbitmq"]["user"] = "admin"
+default["sensu"]["rabbitmq"]["password"] = "password"
