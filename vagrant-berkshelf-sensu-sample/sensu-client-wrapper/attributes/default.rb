4eeda8adcf998106d27c403cdd846b67d9143eb4
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-25 11:57:20 +0900

    move cookbooks to github

--- vagrant-berkshelf-sensu-sample/sensu-client-wrapper/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default["sensu"]["use_ssl"] = false
-default["sensu"]["use_embedded_ruby"] = true 
-default["sensu"]["rabbitmq"]["host"] = "localhost"
-default["sensu"]["rabbitmq"]["port"] = 5672
-default["sensu"]["rabbitmq"]["user"] = "admin"
-default["sensu"]["rabbitmq"]["password"] = "password"
--- vagrant-berkshelf-sensu-sample/sensu-client-wrapper/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'sensu_client-wrapper'
-maintainer       'Ryutaro YOSHIBA'
-maintainer_email 'ryuzee@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures myapp'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          'sensu'
