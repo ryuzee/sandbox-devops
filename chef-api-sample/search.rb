9fbb2d1bb7cbdfc62b2458e578ec0aedfbfd3dae
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-03-23 07:13:55 +0900

    updated

--- chef-api-sample/search.rb
+++ chef-api-sample/search.rb
@@ -6,6 +6,6 @@ require 'chef/search/query'
 
 Chef::Config.from_file(File.expand_path("~/.chef/knife.rb"))
 query = Chef::Search::Query.new
-nodes = query.search('node', 'roles:web AND name:Web*').first rescue []
+nodes = query.search('node', 'roles:web AND chef_environment:production').first rescue []
 
 puts nodes.map(&:name).join("\n")
